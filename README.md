8f1972499c8a60c2e3bb144df33dc520c5372acf
Garrett <gzleising@gmail.com>
2020-10-03 00:22:14 -0700

    everything should be working now, and theres instructions to follow

--- README.md
+++ README.md
@@ -3,15 +3,26 @@ Custom zsh profile with aliases and themes that I use
 
 ## Install
 
-### Step 1: Install OhMyZsh if not already installed
+### Step 1: Clone Repo
+```
+mkdir ~/Projects
+```
+This is so all the the aliases will work
+```
+git clone git@github.com:garrettleising/ZshDotfiles.git ~/Projects/ZshDotfiles
+```
 
+### Step 2: Install OhMyZsh if not already installed
 ```
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ```
 
-### Step 2: Install personal themes and plugins for OhMyZsh
-
+### Step 3: Install personal themes and plugins for OhMyZsh
 ```
 sh install.sh
 ```
 
+### Step 4: Restart Zsh
+```
+exec zsh
+```
--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
 
 cp .zprofile ~/
 cp .zshrc ~/
